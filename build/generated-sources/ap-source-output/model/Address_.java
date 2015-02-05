5250fed0f928635a6232f334a41f31e7846cdbe4
rmsor <rmsor.pth02@gmail.com>
2015-02-05 16:13:28 -0600

    build files

--- build/generated-sources/ap-source-output/model/Address_.java
+++ build/generated-sources/ap-source-output/model/Address_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-01-31T08:30:53")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-02-01T13:53:19")
 @StaticMetamodel(Address.class)
 public class Address_ { 
 
