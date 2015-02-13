849421d16d53179cf280d0f1cebb0175241f56d9
xterememe <m.minesh@live.com>
2015-02-13 14:33:49 -0600

    working on comments

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -1,16 +1,20 @@
 package controller;
 
+import ejb.PostFacade;
 import ejb.UserFacade;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
+import model.Post;
 import model.User;
 
 /**
@@ -19,19 +23,16 @@ import model.User;
  */
 @ManagedBean
 @SessionScoped
-public class LoginController {
+public class LoginController extends BaseController{
+    @EJB
+    private PostFacade posts;
     @EJB
     private UserFacade userFacade;
     private User user;
     private String email;
-    private ExternalContext ec;
+    private List<Post> allPosts;
+    private String comment;
 
-    
-    public LoginController(){
-        ec = FacesContext.getCurrentInstance().getExternalContext();
-        
-    }
-    
     
     public String getEmail() {
         return email;
@@ -54,7 +55,7 @@ public class LoginController {
     
     public String register(){
         try {
-            ec.redirect(ec.getRequestContextPath() + "/faces/pages/user/signup.xhtml");
+            getEc().redirect(getEc().getRequestContextPath() + "/faces/pages/user/signup.xhtml");
         } catch (IOException ex) {
 
         }
@@ -63,9 +64,7 @@ public class LoginController {
     }
     
     public String displayName(){
-        String email = ec.getRemoteUser();
-        
-        System.out.println("email: " + email);
+        String email = getEc().getRemoteUser();
         
         user = userFacade.findByEmail(email);
         
@@ -76,7 +75,7 @@ public class LoginController {
     public String forgotPassword(){
         
         try {
-            ec.redirect(ec.getRequestContextPath() + "/faces/forgotPassword.xhtml");
+            getEc().redirect(getEc().getRequestContextPath() + "/faces/forgotPassword.xhtml");
         } catch (IOException ex) {
 
         }
@@ -116,8 +115,52 @@ public class LoginController {
         return "";
     }
     
+    
+    public String showPosts(){
+        /**
+         * We could do 
+         * List<Post> allPosts = posts.findAll();
+         *  
+         * and then simply 
+         * 
+         * return "showPosts";
+         * 
+         * In showPosts.xhtml, we can easily have allPosts available through the loginController bean
+         * BUT this will cause the servlet to only forward the request not redirect.
+         * 
+         * And if we redirect, the bean will not be available. We will need flash scope to survive a
+         * redirect.
+         * 
+         * More: maxkatz.org/2010/07/27/learning-jsf2-using-flash-scope/
+         */
+       
+        allPosts = posts.findAll();
+        getFlash().put("posts", allPosts);
+        
+        System.out.println("inside the submit commment");
+        return "showPosts?faces-redirect=true";
     }
 
-//http://www.pramati.com/docstore/1270002/index.htm
-//    http://www.avajava.com/tutorials/lessons/how-do-i-create-a-login-module.html
-//    http://stackoverflow.com/questions/20396276/jaas-exception-null-username-and-password
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    
+    
+    
+    public String submitComment(){
+        Logger.getLogger(this.getClass().getName()).log(Level.INFO, this.comment);
+        
+        System.out.println("inside the submit commment");
+        
+        return "dashboard.xhtml?faces-redirect=true";
+    }
+    
+}
+
+// http://www.pramati.com/docstore/1270002/index.htm
+// http://www.avajava.com/tutorials/lessons/how-do-i-create-a-login-module.html
+// http://stackoverflow.com/questions/20396276/jaas-exception-null-username-and-password
--- src/java/model/Post.java
+++ src/java/model/Post.java
@@ -13,6 +13,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -35,7 +37,7 @@ public class Post implements Serializable {
     private Double pricePerMonth;
     private String requiredGender;
     private String requiredCountry;
-    private int minimumAge;    
+    private int minimumAge;
     private int maximumAge;
     private String rommieQualities;
     private List<String> images;
@@ -43,6 +45,10 @@ public class Post implements Serializable {
     @JoinColumn(name = "userId",referencedColumnName = "id")
     @ManyToOne
     private User postedBy;   
+
+	@OneToMany
+    @JoinTable(name="POST_COMMENTS")
+    private List <Comment> comments;
     
     public List<String> getImages() {
         return images;
